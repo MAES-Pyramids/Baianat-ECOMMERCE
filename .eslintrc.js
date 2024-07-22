904d49ee3e9584ff0b81181b829e4d0fb7fe0d12
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2024-07-22 17:08:30 +0300

    intial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  root: true,
+  env: { node: true, jest: true },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'no-console': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-empty-interface': 'warn',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+};
