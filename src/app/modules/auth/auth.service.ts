03a64495f29bdc66383b1b4faa839f289af9085b
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2024-07-23 19:35:39 +0300

    Update validation logic to ensure payload data is up-to-date with database
    - Added a database query in JwtStrategy validate method to ensure user data is current, supporting scenarios where user roles or other attributes may be updated without requiring re-login.
    - Implemented createAuthPayload utility to maintain consistent payload structure.
    - Updated login service and JwtStrategy to use createAuthPayload for consistent access token format.
    - Ensured access token payload consistency when issued by login service or generated by JwtStrategy and mounted on the context.

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { JwtService } from '@nestjs/jwt';
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { AuthPayload, User } from '../../shared/types/graphql.schema';
+import { createAuthPayload } from '../../shared/utils/auth-payload.util';
 
 @Injectable()
 export class AuthService {
@@ -20,7 +21,7 @@ export class AuthService {
 
   async login(user: User): Promise<AuthPayload> {
     const secret = process.env.JWT_SECRET;
-    const jwtPayload = { email: user.email, role: user.role, sub: user.id };
+    const jwtPayload = createAuthPayload(user);
     const options = { expiresIn: process.env.JWT_EXPIRES_IN };
 
     const accessToken = this.jwtService.sign(jwtPayload, {
